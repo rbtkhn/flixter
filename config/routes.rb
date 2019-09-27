41605cd1a289baf51094ef6560109363fd8f46d6
rbtkhn <rbtkhn@protonmail.com>
2019-09-27 14:19:23 +0000

    Lesson 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  	devise_for :users
 	root 'static_pages#index'
+	namespace :instructor do
+		resources :courses, only: [:new, :create, :show]
+	end
 end
  
